bc9f589dad5201582c950632c57705946bced452
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-11 17:16:26 +0900

    feat : Addressable의 에셋을 경로로 불러오기 위한 preset

---
+++ Assets/2.Scripts/Addressable/AddressableEnums.cs
@@ -0,0 +1,22 @@
+/// <summary>addressable group name</summary>
+public enum eAddressableType
+{
+    Animation,
+    Audio,
+    Data,
+    Prefab,
+    Texture,
+    UI
+}
+
+/// <summary>type of file extension</summary>
+public enum eAssetType
+{
+    other,
+    sprite,
+    json,
+    prefab,
+    audio,
+    font,
+    animation
+}
---
+++ Assets/2.Scripts/Addressable/AddressableMap.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class AddressableMapList
+{
+    public List<AddressableMap> list = new List<AddressableMap>();
+
+    public void AddRange(List<AddressableMap> list)
+    {
+        this.list.AddRange(list);
+    }
+
+    public void Add(AddressableMap data)
+    {
+        list.Add(data);
+    }
+}
+
+[Serializable]
+public class AddressableMap
+{
+    public eAddressableType addressableType;
+    public eAssetType assetType;
+    public string key;
+    public string path;
+}
