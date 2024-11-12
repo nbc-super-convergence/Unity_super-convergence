1ede7c8a797d0263b12cb53a4ebf58b596b49aaf
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-12 15:09:15 +0900

    feat : UIManager, UIBase 추가 - UI Prefab 동적 생성

---
+++ Assets/2.Scripts/UI/Core/UIManager.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public enum eUIPosition
+{
+    /// <summary> 0 : 가장 밑에 깔리는 배경화면</summary>
+    Background,
+
+    /// <summary> 1 : 화면 상/하단의 안내 바</summary>
+    Navigator,
+
+    /// <summary> 2 : 팝업창</summary>
+    Popup,
+
+    /// <summary> 3 : 가장 위에서 모든걸 가려주는 UI.</summary>
+    Overlap
+}
+
+public class UIManager : Singleton<UIManager>
+{
+    private List<Transform> parents;
+    private List<UIBase> uiList = new List<UIBase>();
+
+    //GameManager해서 호출함으로써 Manager간 초기화 서순 지키기.
+    public void Init()
+    {
+        isInitialized = true;
+    }
+
+    public static void SetParents(List<Transform> parents)
+    {
+        Instance.parents = parents;
+    }
+
+    /// <typeparam name="T">UIBase를 상속받은 클래스 이름</typeparam>
+    /// <param name="param">원하는 모든 형태의 변수 전달 가능</param>
+    /// <returns>T 반환</returns>
+    public async static Task<T> Show<T>(params object[] param) where T : UIBase
+    {
+        var ui = Instance.uiList.Find(obj => obj.name == typeof(T).ToString());
+
+        if (ui == null)
+        {
+            var prefab = await ResourceManager.Instance.LoadAsset<T>(typeof(T).ToString(), eAddressableType.UI);
+            ui = Instantiate(prefab, Instance.parents[(int)prefab.uiPosition]);
+            ui.name = ui.name.Replace("(Clone)", "");
+
+            Instance.uiList.Add(ui);
+        }
+        ui.opened.Invoke(param);
+        ui.gameObject.SetActive(ui.isActiveInCreated);
+        ui.isActiveInCreated = true;
+        return (T)ui;
+    }
+
+    /// <summary>
+    /// UIBase의 bool값에 따라 setactive false 또는 파괴
+    /// </summary>
+    /// <typeparam name="T">UIBase를 상속받은 클래스 이름</typeparam>
+    /// <param name="param">원하는 모든 형태의 변수 전달 가능</param>
+    public static void Hide<T>(params object[] param) where T : UIBase
+    {
+        var ui = Instance.uiList.Find(obj => obj.name == typeof(T).ToString());
+        if (ui != null)
+        {
+            ui.closed.Invoke(param);
+            if (ui.isDestroyAtClosed)
+            {
+                Instance.uiList.Remove(ui);
+                Destroy(ui.gameObject);
+            }
+            else
+            {
+                ui.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 동적 생성한 UI를 가져오는 메서드
+    /// </summary>
+    /// <typeparam name="T">UI 스크립트 이름</typeparam>
+    /// <returns>UI 스크립트</returns>
+    public static T Get<T>() where T : UIBase
+    {
+        return (T)Instance.uiList.Find(obj => obj.name == typeof(T).ToString());
+    }
+
+    /// <summary>
+    /// UI 존재 여부
+    /// </summary>
+    /// <typeparam name="T">UI 스크립트 이름</typeparam>
+    /// <returns></returns>
+    public static bool IsOpened<T>() where T : UIBase
+    {
+        return Instance.uiList.Exists(obj => obj.name == typeof(T).ToString());
+    }
+}
