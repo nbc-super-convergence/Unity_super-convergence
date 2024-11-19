fae45fe13a9ac680ead73d2c9c53e4a5b0492191
ParkInsu456 <pisis7979@naver.com>
2024-11-19 19:15:49 +0900

    Add : UIError

---
+++ Assets/2.Scripts/UI/UIStartScene/UIDEBUGER.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDEBUGER : MonoBehaviour
+{
+    public Button[] button;
+
+    public void ButtonTest()
+    {
+        TestCode();
+    }
+    public void ButtonTest2()
+    {
+        TestCode2();
+    }
+
+    private async void TestCode()
+    {
+        await UIManager.Show<UIError>("abcv");
+    }
+    private async void TestCode2()
+    {
+        await UIManager.Show<UIError>("이건 어떻게 나올까");
+    }
+
+}
---
+++ Assets/2.Scripts/UI/UIStartScene/UIError.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class UIError : UIBase
+{
+    [SerializeField] private TMP_Text errorMessage;
+
+    private StringBuilder sbError = new();
+
+    //private bool isPopup = false;
+
+    public override void Opened(object[] param)
+    {
+        sbError.Clear();
+        sbError.Append(param[0].ToString());
+        errorMessage.text = sbError.ToString();
+        //isPopup = true;
+        StartCoroutine(Close());
+    }
+
+    public override void Closed(object[] param)
+    {
+        StopCoroutine(Close());
+    }
+
+
+    public async void ShowError(string message) // 또는 에러메세지
+    {
+        await UIManager.Show<UIError>();
+    }
+
+    private IEnumerator Close()
+    {
+        yield return new WaitForSeconds(2f);
+        UIManager.Hide<UIError>();
+    }
+}
