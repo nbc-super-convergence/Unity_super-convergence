10eec3d1f2681a14bb5faea33d4d17931b83490b
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-21 15:46:17 +0900

    update : UIMakeRoom 스크립트 리팩토링

--- Assets/2.Scripts/UI/UIStartScene/Lobby/UIMakeRoom.cs
+++ Assets/2.Scripts/UI/UIStartScene/Lobby/UIMakeRoom.cs
@@ -1,61 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UIMakeRoom : UIBase
 {
-    [SerializeField] private InputField inputFieldRoomName;
-    [SerializeField] private InputField inputFieldPassword;
-    private StringBuilder roomName = new StringBuilder();
-    private StringBuilder roomPassword = new StringBuilder();
+    [SerializeField] private TMP_InputField RoomNameInput;
 
-    [SerializeField]
-    private Button[] buttons;
-
-    [SerializeField]
-    private ToggleGroup maxPlayerToggleGroup;
-
-    private void Start()
-    {
-        InitBtn();
-    }
-
-    private void InitBtn()
-    {
-        buttons[0].onClick.AddListener(Back);
-        buttons[1].onClick.AddListener(Apply);
-    }
-
-    private async void Back()
+    public void OnBackBtn()
     {
         UIManager.Hide<UIMakeRoom>();
-        await UIManager.Show<UILobby>();
     }
 
-    private void Apply()
+    public async void OnApplyBtn()
     {
-        Debug.Log($"Apply is Not Ready");
-    }
-
-    private void GetRoomName()
-    {
-        roomName.Append(inputFieldRoomName.text);
-    }
-    private void GetPassword()
-    {
-        roomName.Append(inputFieldPassword.text);
-    }
-    private int GetToggleValue(ToggleGroup toggleGroup)
-    {
-        foreach (var toggle in toggleGroup.GetComponentsInChildren<Toggle>())
-        {
-            if (toggle.isOn)
-            {
-                return int.Parse(toggle.GetComponentInChildren<Text>().text);
-            }
-        }
-        return 4;
+        UIManager.Hide<UIMakeRoom>();
+        //Send Make Room
+        //πÊ ¿Ã∏ß: RoomNameInput.text 
+        await UIManager.Show<UIRoom>();
+        UIManager.Hide<UILobby>();
     }
 }
