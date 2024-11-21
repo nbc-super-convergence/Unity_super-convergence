66c9e0e2c9c6ab6df43aee960d5275b991b25297
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-11-21 16:50:29 +0900

    Update : 에디터 연속이동기능 추가

--- Assets/2.Scripts/Editor/Board/UpdateInput.cs
+++ Assets/2.Scripts/Editor/Board/UpdateInput.cs
@@ -1,12 +1,15 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 using System;
 
 public class UpdateInput : IInputAction
 {
     private BoardCreator b;
     private Stack<Action> stack = new();
+    private bool isperformed = false;
+    private int performCount = 0;
 
     public UpdateInput(BoardCreator b)
     {
@@ -15,20 +18,49 @@ public class UpdateInput : IInputAction
 
     public void Arrow(InputAction.CallbackContext context)
     {
+        if (CustomCreate.nodes.Count == 0) return;
+
+        string input = context.action.activeControl.ToString();
+
+        int num = 0;
+
+        if (input.Contains("right")) num = 1;
+        else num = -1;
+
         if (context.started)
         {
-            if (CustomCreate.nodes.Count == 0) return;
-
             int index = b.indexs[(int)IndexType.NextNode];
             int count = CustomCreate.nodes.Count;
-            string input = context.action.activeControl.ToString();
-
-            int num = 0;
-
-            if (input.Contains("right")) num = 1;
-            else num = -1;
 
             b.indexs[(int)IndexType.NextNode] = (index + num + count) % count;
+        }
+
+        if(context.performed)
+        {
+            isperformed = true;
+            b.StartCoroutine(Arrow(num));
+        }
+        else if(context.canceled)
+            isperformed = false;
+    }
+
+    public IEnumerator Arrow(int num)
+    {
+        float time = 0.0f;
+        while(isperformed)
+        {
+            time += Time.deltaTime;
+
+            if(time > 0.2f)
+            {
+                int index = b.indexs[(int)IndexType.NextNode];
+                int count = CustomCreate.nodes.Count;
+
+                b.indexs[(int)IndexType.NextNode] = (index + num + count) % count;
+                time = 0.0f;
+            }
+
+            yield return null;
         }
     }
 
@@ -60,18 +92,40 @@ public class UpdateInput : IInputAction
 
     public void WASD(InputAction.CallbackContext context)
     {
-        int index = b.indexs[(int)IndexType.Prefab];
-        int count = CustomCreate.nodes.Count;
         Vector3 input = context.ReadValue<Vector3>();
 
         int num = 0;
 
         if (input == Vector3.right) num = 1;
-        else if(input == Vector3.left) num = -1;
+        else if (input == Vector3.left) num = -1;
 
-        b.indexs[(int)IndexType.Prefab] = (index + num + count) % count;
+
+        performCount += 1;
+        if (performCount % 2 == 1)
+            b.StartCoroutine(WASD(num));
     }
 
+    public IEnumerator WASD(int num)
+    {
+        float time = 0.0f;
+
+        while (performCount % 2 == 1)
+        {
+            Debug.Log(1);
+            time += Time.deltaTime;
+
+            if (time > 0.2f)
+            {
+                int index = b.indexs[(int)IndexType.Prefab];
+                int count = CustomCreate.nodes.Count;
+
+                b.indexs[(int)IndexType.Prefab] = (index + num + count) % count;
+                time = 0.0f;
+            }
+
+            yield return null;
+        }
+    }
     public void InputEnter()
     {
         b.actions[(int)InputType.WASD] += WASD;
