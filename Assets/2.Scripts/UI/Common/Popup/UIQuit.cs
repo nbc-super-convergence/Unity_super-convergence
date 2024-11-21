517bba5987f588ebc8da86f4268f41929c6c6d03
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-21 21:21:16 +0900

    feat : 게임 종료 판넬

---
+++ Assets/2.Scripts/UI/Common/Popup/UIQuit.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+
+public class UIQuit : UIBase
+{
+    //Inspector: 돌아가기
+    public void OnBackBtn()
+    {
+        UIManager.Hide<UIQuit>();
+    }
+
+    //Inspector: 진짜 게임종료
+    public void OnQuitBtn()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
