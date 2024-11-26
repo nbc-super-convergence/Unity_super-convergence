7d6a9fae6c739a8906354a76118e69f40aaf3175
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-26 20:31:35 +0900

    ,

--- Assets/2.Scripts/testScript.cs
+++ Assets/2.Scripts/testScript.cs
@@ -6,4 +6,8 @@ public class testScript : MonoBehaviour
     public TextMeshProUGUI Text1;
     public TextMeshProUGUI Text2;
 
+    private void Update()
+    {
+        Text1.text = GameManager.Instance.PlayerId.ToString();
+    }
 }
