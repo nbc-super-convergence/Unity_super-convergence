11fd869daf3c0ffe9dc03fe958d26510af068dc7
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-27 20:07:56 +0900

    feat : Animation 전환 단순화

---
+++ Assets/2.Scripts/Util/AnimState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+public static class AnimState
+{
+    #region Animation Hashes
+    private static readonly int IdleHash = Animator.StringToHash("Idle");
+    private static readonly int MoveHash = Animator.StringToHash("Move");
+    private static readonly int JumpHash = Animator.StringToHash("Jump");
+    private static readonly int DieHash = Animator.StringToHash("Die");
+    #endregion
+
+    public static void ChangePlayerAnimState(Animator player, State state)
+    {
+        switch(state)
+        {
+            case State.Idle:
+                player.SetTrigger(IdleHash);
+                break;
+            case State.Move:
+                player.SetTrigger(MoveHash);
+                break;
+            case State.Die:
+                player.SetTrigger(DieHash);
+                break;
+        }
+    }
+}
