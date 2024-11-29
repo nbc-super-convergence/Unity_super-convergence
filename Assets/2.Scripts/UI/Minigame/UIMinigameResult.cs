fef9760353d76a8afc1eff38e06adedfdd189153
Chloe <144107013+Charen523@users.noreply.github.com>
2024-11-29 20:49:39 +0900

    update : result 패널 &GameEnd 연결

--- Assets/2.Scripts/UI/Minigame/UIMinigameResult.cs
+++ Assets/2.Scripts/UI/Minigame/UIMinigameResult.cs
@@ -1,4 +1,11 @@
 public class UIMinigameResult : UIBase
 {
-
+    public override void Opened(object[] param)
+    {
+        Rank[] ranks = (Rank[])param;
+        foreach (var r in ranks)
+        {
+            //SessionId받아서 Rank 표시.
+        }
+    }
 }
