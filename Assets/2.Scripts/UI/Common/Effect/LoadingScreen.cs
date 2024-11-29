3462abfd0990ec2791e8c189f241da5cd9a7d6ee
ParkInsu456 <pisis7979@naver.com>
2024-11-29 17:16:56 +0900

    add : LoadingScreen

---
+++ Assets/2.Scripts/UI/Common/Effect/LoadingScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] private RectTransform image;
+    public float rotateSpeed = 200f;
+
+    private void FixedUpdate()
+    {
+        image.Rotate(0f, (rotateSpeed * Time.deltaTime), 0f);
+    }
+
+
+}
