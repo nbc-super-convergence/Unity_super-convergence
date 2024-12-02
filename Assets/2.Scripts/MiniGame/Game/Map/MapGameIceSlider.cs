2277263584b5fca8cf0c82a77d9403be437db107
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-02 12:37:37 +0900

    feat : Map 상호작용 로직

---
+++ Assets/2.Scripts/MiniGame/Game/Map/MapGameIceSlider.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+public class MapGameIceSlider : MapBase
+{
+    [SerializeField] private Transform iceBoard;
+    [SerializeField] private GameObject oceanBoard;
+
+    [SerializeField] private float phaseTime = 1f;
+    [SerializeField] private float bounceForce = 10f;
+    [SerializeField] private float inputDelay = 1f;
+
+    public override void HandleCollision(Collision collision, eCollisionType type)
+    {
+        switch (type)
+        {
+            case eCollisionType.Bounce:
+                // 충돌 방향 계산
+                Vector3 collisionNormal = collision.contacts[0].normal;
+                Vector3 bounceDirection = -collisionNormal.normalized;
+
+                collision.rigidbody.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
+                collision.gameObject.GetComponent<MiniToken>().PausePlayerInput(inputDelay);
+                break;
+            case eCollisionType.Damage:
+                MinigameManager.Instance.GetMiniGame<GameIceSlider>()
+                    .GiveDamage(MinigameManager.Instance.MySessonId, 1, true);
+                break;
+        }
+    }
+
+    public void MapDecreaseEvent(int phase)
+    {
+        StartCoroutine(DecreaseSize(phase));
+    }
+
+    private IEnumerator DecreaseSize(int phase)
+    {
+        Vector3 startScale = transform.localScale;
+        Vector3 targetSize = new Vector3(phase, 1, phase);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < phaseTime)
+        {
+            transform.localScale = Vector3.Lerp(startScale, targetSize, elapsedTime / phaseTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = targetSize;
+    }
+}
