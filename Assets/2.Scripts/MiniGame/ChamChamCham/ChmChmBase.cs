83495e54a3ca042edf35b1a64d7236caa9176d8d
RCO8 <gurwo63@naver.com>
2024-12-03 12:38:17 +0900

    wait : 블라인더 동작 임시 구현

--- Assets/2.Scripts/MiniGame/ChamChamCham/ChmChmBase.cs
+++ Assets/2.Scripts/MiniGame/ChamChamCham/ChmChmBase.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+
 
 public class ChmChmBase : MonoBehaviour
 {
-    Vector3 lookingDirection = Vector3.zero;    //플레이어가 바라보는 방향
+    [SerializeField] private GameObject ownerBlinder;
+    [SerializeField] private GameObject[] otherBlinder;
 
-
-    /// <summary>
-    /// 입력을 받아서 lookingDirection에 적용
-    /// </summary>
-    /// <param name="dir">Vector3지만 좌우 방향으로만 바라보게</param>
-    public void InputDirection(Vector3 dir)
-    {
-        lookingDirection = dir;
-    }
+    public enum YourRole { Owner, Other };
+    public YourRole MyRole;
 }
