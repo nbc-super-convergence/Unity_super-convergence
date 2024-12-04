7f12247d44bd56ce4ea6e6eccdd482603ece2ce2
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-12-04 13:56:31 +0900

    Update : 보드 통신부분 업데이트

--- Assets/2.Scripts/Network/Core/SocketManager_Board.cs
+++ Assets/2.Scripts/Network/Core/SocketManager_Board.cs
@@ -4,6 +4,7 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
 {
     #region 보드
 
+    #region 주사위
     public void RollDiceResponse(GamePacket packet)
     {
         var response = packet.RollDiceResponse;
@@ -32,6 +33,9 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
         Debug.Log("RollDiceNotification");
     }
 
+    #endregion
+
+    #region 보드 플레이어 이동
     public void MovePlayerBoardResponse(GamePacket packet)
     {
         var response = packet.MovePlayerBoardResponse;
@@ -59,6 +63,9 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
         Debug.Log("MovePlayerBoardNotification");
     }
 
+    #endregion
+
+    #region 타일 구매
     public void PurchaseTileResponse(GamePacket packet)
     {
         var response = packet.PurchaseTileResponse;
@@ -66,11 +73,11 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
         if (response.Success)
         {
             int i = response.Tile;
-            var player = BoardManager.Instance.Curplayer;
-            int p = BoardManager.Instance.Curplayer.data.userInfo.Order;
-            int c = BoardManager.Instance.Curplayer.data.userInfo.Color;
 
-            BoardManager.Instance.areaNodes[i].SetArea(p,c);
+            var player = BoardManager.Instance.Curplayer.data;
+            string id = player.userInfo.SessionId;
+
+            BoardManager.Instance.areaNodes[i].SetArea(id);
         }
         else
         {
@@ -82,13 +89,74 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
     {
         var response = packet.PurchaseTileNotification;
 
-        var player = BoardManager.Instance.GetToken(response.SessionId);
-        int i = response.Tile;
-        int p = player.data.userInfo.Order;
-        int c = player.data.userInfo.Color;
+        string id = response.SessionId;
 
-        BoardManager.Instance.areaNodes[i].SetArea(p, c);
+        int i = response.Tile;
+        var player = BoardManager.Instance.GetToken(id);
+
+        BoardManager.Instance.areaNodes[i].SetArea(id);
     }
+    #endregion
+
+    #region 트로피 구매
+    public void PurchaseTrophyResponse(GamePacket packet)
+    {
+        var response = packet.PurchaseTrophyResponse;
+
+        if(response.Success)
+        {
+            int i = response.NextTile;
+
+            var list = BoardManager.Instance.trophyNode;
+            list[i].Toggle();
+        }
+        else
+        {
+            Debug.LogError($"FailCode : {response.FailCode.ToString()}");
+        }
+    }
+    
+    public void PurchaseTrophyNotification(GamePacket packet)
+    {
+        var response = packet.PurchaseTrophyNotification;
+
+        string id = response.SessionId;
+        var player = BoardManager.Instance.GetToken(id).data;
+        player.trophyAmount += 1;
+
+        int i = response.BeforeTile;
+        int j = response.NextTile;
+
+        BoardManager.Instance.trophyNode[i].Toggle();
+        BoardManager.Instance.trophyNode[j].Toggle();
+    }
+
+    #endregion
+
+    #region 타일 패널티
+
+    public void TilePenaltyRequest(GamePacket packet)
+    {
+        var response = packet.TilePenaltyResponse;
+
+        if (response.Success)
+        {
+
+        }
+        else
+        {
+            Debug.LogError($"FailCode : {response.FailCode.ToString()}");
+        }
+    }
+
+    public void TilePenaltyResponse(GamePacket packet)
+    {
+        var response = packet.TilePenaltyNotification;
+
+    }
+
+    #endregion
+
 
     #endregion
 
