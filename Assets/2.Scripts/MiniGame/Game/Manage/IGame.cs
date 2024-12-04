566bcb79f65c60c3cbb1dfdd7af8432833844d79
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-04 18:05:30 +0900

    update : IGame에서 게임종료 로직 공통화

--- Assets/2.Scripts/MiniGame/Game/Manage/IGame.cs
+++ Assets/2.Scripts/MiniGame/Game/Manage/IGame.cs
@@ -1,8 +1,17 @@
+using System;
 using System.Collections.Generic;
 
 public interface IGame
 {
     public void Init(params object[] param);
     public void GameStart();
-    public void GameEnd(Dictionary<string, int> ranks);
+    public async void GameEnd(Dictionary<string, int> ranks, DateTime returnBoardTime)
+    {
+        foreach (var mini in MinigameManager.Instance.MiniTokens)
+        {
+            mini.gameObject.SetActive(false);
+        }
+
+        await UIManager.Show<UIMinigameResult>(ranks, returnBoardTime);
+    }
 }
