fd409788abe7c3fb9a489614794bce5faba007da
RCO8 <gurwo63@naver.com>
2024-12-05 19:13:29 +0900

    update 텍스트 색상 추가

--- Assets/2.Scripts/SelectOrder/SelectOrderResultUI.cs
+++ Assets/2.Scripts/SelectOrder/SelectOrderResultUI.cs
@@ -1,24 +1,51 @@
 ﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectOrderResultUI : MonoBehaviour
 {
-    public List<TextMeshProUGUI> resultTexts;
+    public int player = 1;
+    private Image bgImage;
+    private TextMeshProUGUI stateText;
+    public string Nickname { get; private set; }
+    private string state;
 
-    public void SetDistanceUI(int idx, float dist)
+    // 닉네임 : 상태 (준비중 / 내차례 / 거리)
+
+    private void Awake()
     {
-        resultTexts[idx].text = $"{idx + 1}P : {dist.ToString("N4")}";
-        switch(idx)
-        {
-            case 0:
-                resultTexts[idx].color = Color.red; break;
-            case 1:
-                resultTexts[idx].color = Color.yellow; break;
-            case 2:
-                resultTexts[idx].color = Color.green; break;
-            case 3:
-                resultTexts[idx].color = Color.blue; break;
-        }
+        bgImage = GetComponent<Image>();
+        stateText = GetComponentInChildren<TextMeshProUGUI>();
     }
+
+    private void Start()
+    {
+        bgImage.color = Color.gray;
+        
+        switch(player)  //플레이어 색상
+        {
+            case 1: stateText.color = Color.red; break;
+            case 2: stateText.color = Color.yellow; break;
+            case 3: stateText.color = Color.green; break;
+            case 4: stateText.color = Color.blue; break;
+            default: stateText.color = Color.gray; break;
+        }
+
+        SetReady();
+    }
+
+    private void Update()
+    {
+        ApplyText();
+    }
+
+    private void ApplyText()
+    {
+        stateText.text = $"{Nickname} : {state}";
+    }
+
+    public void SetReady() => state = "준비";
+    public void SetMyTurn() => state = "내 차례";
+    public void SetFinish(float dist) => state = dist.ToString("N4");
 }
