1d58759c765e4a937689097ce69d7080aba13bf3
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-05 16:29:35 +0900

    update : 불필요 코드삭제, 필요한 것만 초기화 코드 작성

--- Assets/2.Scripts/MiniGame/Game/Data/GameDropperData.cs
+++ Assets/2.Scripts/MiniGame/Game/Data/GameDropperData.cs
@@ -1,12 +1,9 @@
 public class GameDropperData : IGameData
 {
-    public float totalTime;
-    public int maxHP;
-    public float[] playerHps = new float[4];
     public int phase;
 
     public void Init()
     {
-        
+        phase = 0;
     }
 }
