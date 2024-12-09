c6abbbd78d4a7df8d8121a4efaa658a4397e6192
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-09 17:50:31 +0900

    update : socketManager 레벨 제외 구현

--- Assets/2.Scripts/Network/Core/SocketManager_Dropper.cs
+++ Assets/2.Scripts/Network/Core/SocketManager_Dropper.cs
@@ -1,9 +1,20 @@
+using System.Collections.Generic;
+
 public partial class SocketManager : TCPSocketManagerBase<SocketManager>
 {
     //301 
     public void DropMiniGameReadyNotification(GamePacket gamePacket)
     {
+        var response = gamePacket.DropMiniGameReadyNotification;
 
+        //ReadyPanel 띄우기.
+#pragma warning disable CS4014 
+        UIManager.Show<UIMinigameReady>(eGameType.GameDropper);
+#pragma warning restore CS4014
+
+        //데이터 설정, 맵 설정, BGM 설정
+        MinigameManager.Instance.SetMiniGame<GameDropper>(response);
+        MinigameManager.Instance.boardCamera.SetActive(false);
     }
 
     //302 dropGameReadyRequest
@@ -11,13 +22,19 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
     //303
     public void DropGameReadyNotification(GamePacket gamePacket)
     {
+        var response = gamePacket.DropGameReadyNotification;
 
+        //ReadyUI와 연계
+        UIManager.Get<UIMinigameReady>().SetReady(response.SessionId);
     }
 
     //304
     public void DropMiniGameStartNotification(GamePacket gamePacket)
     {
-
+        //ReadyUI 숨기기
+        UIManager.Hide<UIMinigameReady>();
+        //GameStart 함수 호출
+        MinigameManager.Instance.GetMiniGame<GameDropper>().GameStart();
     }
 
     //305 DropPlayerSyncRequest
@@ -25,13 +42,19 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
     //306
     public void DropPlayerSyncNotification(GamePacket gamePacket)
     {
+        var response = gamePacket.DropPlayerSyncNotification;
 
+        MinigameManager.Instance.GetMiniGame<GameDropper>()
+            .ReceiveMove(response.SessionId, response.Slot, response.Rotation, response.State);
     }
 
     //307
     public void DropPlayerDeathNotification(GamePacket gamePacket)
     {
+        var response = gamePacket.IcePlayerDeathNotification;
 
+        //플레이어 사망 이벤트
+        MinigameManager.Instance.GetMiniGame<GameDropper>().PlayerDeath(response.SessionId);
     }
 
     //308
@@ -43,12 +66,26 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
     //309
     public void DropLevelEndNotification(GamePacket gamePacket)
     {
-
+        
     }
 
     //310
     public void DropGameOverNotification(GamePacket gamePacket)
     {
+        var response = gamePacket.DropGameOverNotification;
 
+        /*필요 데이터 파싱*/
+        Dictionary<string, int> rankings = new();
+        foreach (var r in response.Ranks)
+        {
+            rankings.Add(r.SessionId, r.Rank_);
+        }
+
+        //UI Minigame Result 판넬 호출
+        MinigameManager.Instance.curMiniGame.GameEnd(rankings, response.EndTime);
+
+        //미니게임 맵 삭제
+        MinigameManager.Instance.boardCamera.SetActive(true);
+        Destroy(MinigameManager.Instance.curMap.gameObject);
     }
 }
