7cf0943b5c296bcd2ff4c13cb9f1981eb92389a1
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-10 13:07:12 +0900

    update : dropper map 초안

--- Assets/2.Scripts/MiniGame/Game/Map/MapgameDropper.cs
+++ Assets/2.Scripts/MiniGame/Game/Map/MapgameDropper.cs
@@ -9,8 +9,48 @@ public class MapGameDropper : MapBase
 
     [SerializeField] private float cameraFollowDelay = 0.2f;
 
-    public void NextLevelEvent(int phase, int[] holes)
+    private GameDropperData gameData;
+    private int prevPhase;
+
+    private void Start()
     {
+        gameData = MinigameManager.Instance.GetMiniGame<GameDropper>().gameData;
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator StartGame()
+    {
+        yield return new WaitUntil(() => gameData.phase == 1);
+        prevPhase = gameData.phase;
+
+        levels[0].SetActive(false);
+        StartCoroutine(MoveCamera());
+    }
+
+    public void NextLevelEvent(int[] holes)
+    {
+        //바닥 없애기.
+        foreach (int hole in holes)
+        {
+            levels[gameData.phase].transform.GetChild(hole).gameObject.SetActive(false);
+        }
+        gameData.phase++;
+        StartCoroutine(MoveCamera());
+    }
+
+    private IEnumerator MoveCamera()
+    {
+        yield return new WaitForSeconds(cameraFollowDelay);
+        
+        Vector3 curPos = gameCamera.transform.position;
+        Vector3 nextPos = new Vector3(curPos.x, curPos.y - 10, curPos.z);
+
+        while (curPos == nextPos)
+        {
+            curPos = Vector3.Lerp(curPos, nextPos, 0.2f);
+            gameCamera.transform.position = curPos;
+            yield return null;
+        }
         
     }
 }
