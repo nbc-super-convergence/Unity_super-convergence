9ec4515f58efb1a15b610d0bdd8504b8792ead3d
RCO8 <gurwo63@naver.com>
2024-12-12 14:06:15 +0900

    update : DartMinigameStartNotification 추가

--- Assets/2.Scripts/Network/Core/SocketManager_Dart.cs
+++ Assets/2.Scripts/Network/Core/SocketManager_Dart.cs
@@ -28,7 +28,8 @@ public class SocketManager_Dart : TCPSocketManagerBase<SocketManager>
 
     public void DartMinigameStartNotification(GamePacket gamePacket)
     {
-
+        var response = gamePacket.DartMiniGameStartNotification;
+        Debug.Log(response);
     }
 
     public void DartGameThrowNotification(GamePacket gamePacket)
@@ -36,6 +37,7 @@ public class SocketManager_Dart : TCPSocketManagerBase<SocketManager>
         var response = gamePacket.DartGameThrowNotification;
         Debug.Log(response);
 
+        //사용자가 아닌 상대라면 response의 데이터를 받아 던지기 신호
 
     }
 
