dd968a47e2ec05bec4859b3bb3ea3f4a09ec2b91
RCO8 <gurwo63@naver.com>
2024-12-12 13:56:22 +0900

    add : 다트게임 세션 추가(?)

--- Assets/2.Scripts/UI/Minigame/UIMinigameReady.cs
+++ Assets/2.Scripts/UI/Minigame/UIMinigameReady.cs
@@ -118,6 +118,12 @@ public class UIMinigameReady : UIBase
                             SessionId = GameManager.Instance.myInfo.SessionId
                         };
                         break;
+                    case eGameType.GameDart:
+                        packet.DartGameReadyNotification = new()
+                        {
+                            SessionId = GameManager.Instance.myInfo.SessionId
+                        };
+                        break;
                 }
 
                 SocketManager.Instance.OnSend(packet);
