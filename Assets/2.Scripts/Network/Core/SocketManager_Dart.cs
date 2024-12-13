1b26bd523b9da4c19675781381cb1553f749a298
RCO8 <gurwo63@naver.com>
2024-12-13 23:33:14 +0900

    update : Start, GameOver 기능 구현

--- Assets/2.Scripts/Network/Core/SocketManager_Dart.cs
+++ Assets/2.Scripts/Network/Core/SocketManager_Dart.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public partial class SocketManager : TCPSocketManagerBase<SocketManager>
@@ -26,7 +27,7 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
 
     //필요한것 : Dart의 방향이 움직이는 동기화, 판도 서버에서 동기화 시키기
 
-    public void DartMinigameStartNotification(GamePacket gamePacket)
+    public void DartMiniGameStartNotification(GamePacket gamePacket)
     {
         //ReadyUI 숨기기
         UIManager.Hide<UIMinigameReady>();
@@ -57,17 +58,39 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
     {
         var response = gamePacket.DartGameOverNotification;
         Debug.Log(response);
+
+        Dictionary<string, int> rankings = new();
+        foreach(var r in response.Ranks)
+        {
+            rankings.Add(r.SessionId, r.Rank_);
+        }
+
+        //UI Minigame Result 판넬 호출
+        MinigameManager.Instance.curMiniGame.GameEnd(rankings, response.EndTime);
+
+        //미니게임 맵 삭제
+        MinigameManager.Instance.boardCamera.SetActive(true);
+        Destroy(MinigameManager.Instance.curMap.gameObject);
     }
 
     public void DartPannelSyncNotification(GamePacket gamePacket)
     {
         var response = gamePacket.DartPannelSyncNotification;
         Debug.Log(response);
+
+
     }
 
     public void DartSyncNotification(GamePacket gamePacket)
     {
         var response = gamePacket.DartSyncNotification;
         Debug.Log(response);
+
+        string sessionId = GameManager.Instance.myInfo.SessionId;
+        int userIdx = GameManager.Instance.SessionDic[sessionId].Color;
+
+        DartPlayer dartUser = MinigameManager.Instance.GetMap<MapGameDart>().DartOrder[userIdx];
+
+        dartUser.CurAim = ToVector3(response.Angle);
     }
 }
