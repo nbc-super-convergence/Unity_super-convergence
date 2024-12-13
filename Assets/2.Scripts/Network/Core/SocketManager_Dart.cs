98bb88ad0f2040afec07ffd05cf1b1299b2ec42c
RCO8 <gurwo63@naver.com>
2024-12-13 15:51:24 +0900

    fix : 소켓매니저, 프리팹 수정

--- Assets/2.Scripts/Network/Core/SocketManager_Dart.cs
+++ Assets/2.Scripts/Network/Core/SocketManager_Dart.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SocketManager_Dart : TCPSocketManagerBase<SocketManager>
+public partial class SocketManager : TCPSocketManagerBase<SocketManager>
 {
     public void DartMiniGameReadyNotification(GamePacket gamePacket)
     {
