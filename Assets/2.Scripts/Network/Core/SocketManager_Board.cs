341ad74a9dd1277800c9078414013011bac510e4
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-13 14:55:55 +0900

    update : 오디오 설정중

--- Assets/2.Scripts/Network/Core/SocketManager_Board.cs
+++ Assets/2.Scripts/Network/Core/SocketManager_Board.cs
@@ -251,11 +251,10 @@ public partial class SocketManager : TCPSocketManagerBase<SocketManager>
     {
         SceneManager.LoadScene(0);
 
-        yield return new WaitUntil(() => SceneManager.GetActiveScene().buildIndex == 0 && StartCanvas.isSet);
+        //yield return new WaitUntil(() => SceneManager.GetActiveScene().buildIndex == 0 && StartCanvas.isSet);
 
         yield return UIManager.Show<UIRoom>(response.Room);
     }
-
     #endregion
 
 }
