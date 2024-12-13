b11a0b406c9aee1a76163ba23155fdeb923aa7da
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-13 17:13:29 +0900

    update : Result 판넬 초기화

--- Assets/2.Scripts/UI/Minigame/UIMinigameResult.cs
+++ Assets/2.Scripts/UI/Minigame/UIMinigameResult.cs
@@ -12,11 +12,14 @@ public class UIMinigameResult : UIBase
     [SerializeField] private Image[] RankPanels;
     [SerializeField] private Sprite[] RankPanelsSprites;
     [SerializeField] private TextMeshProUGUI[] RankTxts;
+    [SerializeField] private TextMeshProUGUI[] CoinTxts;
     [SerializeField] private TextMeshProUGUI returnTxt;
 
     private Dictionary<int, int> colorIdxs = new();
     private Dictionary<int, int> coinDics = new()
     { { 1, 20 }, { 2, 10 }, { 3, 5 }, { 4, 1 } };
+
+    private Sequence titleSequence;
 
     public override void Opened(object[] param)
     {
@@ -33,31 +36,38 @@ public class UIMinigameResult : UIBase
         {
             if (param[0] is Dictionary<string, int> ranks)
             {
+                int i = 0;
                 foreach (var rank in ranks)
                 {
                     string sessionid = rank.Key; //id
                     int rankNum = rank.Value; //등수
                     int color = GameManager.Instance.SessionDic[sessionid].Color; //색깔
-                    colorIdxs.Add(color, rankNum);
+                    colorIdxs.Add(color, rankNum); 
 
-                    RankPanels[color].gameObject.SetActive(true);
+                    RankPanels[i].gameObject.SetActive(true);
 
                     //등수에 맞는 위치에 색깔 지정
-                    RankPanels[rankNum - 1].sprite = RankPanelsSprites[color];
+                    RankPanels[i].sprite = RankPanelsSprites[color];
 
                     //등수 + 닉네임 설정
                     RankTxts[rankNum - 1].text = $"{rankNum}등\n{GameManager.Instance.SessionDic[sessionid].Nickname}";
 
-                    //보상 지급 - 서버에서 처리
-                    //BoardManager.Instance.playerTokenHandlers[color].data.keyAmount += coinDics[rankNum];
+                    //받을 금액 설정
+                    string coin = rankNum switch
+                    {
+                        1 => "+20",
+                        2 => "+10",
+                        3 => "+5",
+                        4 => "+1",
+                        _ => "0"
+                    };
 
-                    //미니게임 순서 재정의
-                    GameManager.Instance.SessionDic[sessionid].SetOrder(rankNum - 1);
+                    i++;
                 }
             }
             else
             {
-                Debug.LogError("param 오류 : idx0이 ranks가 아님");
+                Debug.LogError("param parsing error : ranks");
             }
 
             if (param[1] is long returnTime)
@@ -71,15 +81,16 @@ public class UIMinigameResult : UIBase
             Debug.LogError("param 오류 : object[] length가 다름");
         }
 
-        Sequence sequence = DOTween.Sequence();
-        sequence.Append(titleText.transform.DOScale(1.5f, 0.5f).SetEase(Ease.OutBack));
-        sequence.Append(titleText.transform.DOScale(1f, 0.3f).SetEase(Ease.InOutBounce));
-        sequence.Join(titleText.transform.DOShakePosition(0.3f, new Vector3(10f, 0, 0), 20, 90, false, true));
+        titleSequence = DOTween.Sequence();
+        titleSequence.Append(titleText.transform.DOScale(1.5f, 0.5f).SetEase(Ease.OutBack));
+        titleSequence.Append(titleText.transform.DOScale(1f, 0.3f).SetEase(Ease.InOutBounce));
+        titleSequence.Join(titleText.transform.DOShakePosition(0.3f, new Vector3(10f, 0, 0), 20, 90, false, true));
     }
 
     public override async void Closed(object[] param)
     {
-        base.Closed(param);
+        titleSequence.Kill();
+
         colorIdxs.Clear();
         GameManager.OnPlayerLeft -= PlayerLeftEvent;
 
