6b72ce7b959dbac296d5797914f2e9f04e28dd78
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-13 14:56:18 +0900

    chore : 주석

--- Assets/2.Scripts/MiniGame/Game/Manage/GameDropper.cs
+++ Assets/2.Scripts/MiniGame/Game/Manage/GameDropper.cs
@@ -51,7 +51,7 @@ public class GameDropper : IGame
     #region 초기화
     private void SetBGM()
     {
-        SoundManager.Instance.PlayBGM(BGMType.Dropper);
+        //SoundManager.Instance.PlayBGM(BGMType.Dropper);
     }
 
     private void ResetPlayers(RepeatedField<S2C_DropMiniGameReadyNotification.Types.startPlayers> players)
