f2324490104aa41cc59c7d6c30d8bf15cf74040d
Chloe <144107013+Charen523@users.noreply.github.com>
2024-12-17 22:02:18 +0900

    update : p3, p4에 접근 못하게 하기

--- Assets/2.Scripts/MiniGame/Game/Manage/GameIceSlider.cs
+++ Assets/2.Scripts/MiniGame/Game/Manage/GameIceSlider.cs
@@ -31,7 +31,7 @@ public class GameIceSlider : IGame
 
         foreach (var miniToken in MinigameManager.Instance.miniTokens)
         {
-            miniToken.Controller.ToggleFreezePos(true); 
+ ddd            miniToken.Controller.ToggleFreezePos(true); 
         }
     }
     #endregion
@@ -47,6 +47,8 @@ public class GameIceSlider : IGame
         foreach (var p in players)
         {//미니 토큰 위치 초기화
             MiniToken miniToken = MinigameManager.Instance.GetMiniToken(p.SessionId);
+            if (miniToken == null) continue;
+
             miniToken.EnableMiniToken();
             miniToken.transform.localPosition = SocketManager.ToVector3(p.Position);
             miniToken.MiniData.nextPos = SocketManager.ToVector3(p.Position);
