b692af9a3cf16790e73eb7196c86accc052caa45
RCO8 <gurwo63@naver.com>
2024-12-20 11:36:36 +0900

    update : 점수 패킷 전송

--- Assets/2.Scripts/SelectOrder/GameDartScore.cs
+++ Assets/2.Scripts/SelectOrder/GameDartScore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 public class GameDartScore
@@ -19,6 +20,21 @@ public class GameDartScore
         // TODO:: 점수표UI를 갱신시키는 코드
         UIManager.Get<UIMinigameDart>().AddScore(color, scoreInfo[color].Count, distance);
 
+        float allScore = 0f;
+        foreach(float score in scoreInfo[color])
+        {
+            allScore += score;
+        }
+
         // TODO:: C2S_DartPointRequest 패킷 보내는 코드
+        GamePacket packet = new()
+        {
+            DartPointRequest = new()
+            {
+                SessionId = sessionId,
+                Point = Convert.ToInt16(allScore)
+            }
+        };
+        SocketManager.Instance.OnSend(packet);
     }
 }
